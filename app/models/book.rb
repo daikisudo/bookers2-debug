bb8681636e5168f6ca1cbd1bdbfe271e8166d043
daikisudo <kanigiusa2032@gmail.com>
2020-11-04 17:20:56 +0000

    いいね機能完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,6 @@ class Book < ApplicationRecord
 	
  	belongs_to :user, optional: true
  	has_many :favorites
-def favorited_by?(user)
-    favorites.where(user_id: user.id).exists?
-end
+ 	has_many :favorite_users, through: :favorites, source: 'user'
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
 
   root 'home#top'
-
-
+  get 'home/about'
   
-  get 'home/about' 
-
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
   
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
-  	resources :favorites , only: [:create , :destroy]
+  	resources :favorites, only: [:create, :destroy]
   end
-  
   resources :homes,only:[:top,:about]
  
  end
